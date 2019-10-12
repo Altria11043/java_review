e4c9b1ee5482db79253b9f5d1c3a623c54502b1c
Altria <51522497+Altria11043@users.noreply.github.com>
2019-10-13 02:29:04 +0800

    2019年10月13日 02点28分 了解了类加载器的部分原理 明天继续深入学习

--- src/com/zca/TestJVM/Demo01.java
+++ src/com/zca/TestJVM/Demo01.java
@@ -7,21 +7,51 @@ package com.zca.TestJVM;
  */
 
 public class Demo01 {
-    public static void main(String[] args) {
-        System.out.println("width: " + A.width);
-        A a = new A();
-        System.out.println("width: " + A.width);
+    static{
+        System.out.println("静态初始化块Demo01---->start");
+    }
+    // 此处是方法调用, 必须要先加载完类之后才会调用main方法
+    public static void main(String[] args) throws ClassNotFoundException {
+//        System.out.println(A.height);// 被动调用, A没有被初始化
+        // 主动调用A中的static属性, A被初始化, 但是没有调用A的构造器
+//        System.out.println("width: " + A.width);
+//        new A();// 第二次被主动调用, 没有再次初始化, 而是直接调用了构造器
+
+        // 反射调用
+//        Class.forName("com.zca.TestJVM.A");// 反射调用也会初始化A类
+
+        // 通过数组定义类引用, 不会触发此类的初始化
+//        A[] as = new A[2];
+
+        // 这里调用了B的父类的static属性, B没有被初始化, 而是初始化了实际被调用的A
+        System.out.println(B.width);
+    }
+    static{
+        System.out.println("静态初始化块Demo01---->end");
     }
 }
 
-class A{
-    public static int width = 100;
+class B extends A{
     static {
-        System.out.println("静态块初始化1");
+        System.out.println("B类的静态块初始化");
+    }
+}
+
+class A extends A_Father{
+    public static int width = 100;
+    public static final int height = 500;
+    static {
+        System.out.println("A类的静态块初始化");
         width = 200;
     }
 
     public A(){
         System.out.println("创建A类的对象");
+    }
+}
+
+class A_Father{
+    static{
+        System.out.println("静态初始化A的父类");
     }
 }
